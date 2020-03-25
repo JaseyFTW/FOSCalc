5b0a3d131e4b7e5d75b2756b8b0cfc0d5e58afa8
JaseyFTW <jason.whittaker19@gmail.com>
2020-03-25 15:23:29 +0000

    refered BH order

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function handleDates(data) {
   console.log (england);
   //console.log ("last again");
 
-  for(let i = 0; i < england.length; i++){
+  for(let i = england.length-1; i >= 0; i--){
     JustDates.push(dayjs(england[i].date,"YYYY-MM-DD"));
   }
   console.log(JustDates);
